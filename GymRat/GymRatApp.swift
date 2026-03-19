fdd98bf0948f1f10b3e04a643934bb893a716b40
manoj-learns <manojprakashpeddada@gmail.com>
2026-03-19 23:22:19 +0530

    Initial Commit

---
+++ GymRat/GymRatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymRatApp.swift
+//  GymRat
+//
+//  Created by Manoj Peddada on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GymRatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymRatTests/GymRatTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymRatTests.swift
+//  GymRatTests
+//
+//  Created by Manoj Peddada on 19/03/26.
+//
+
+import Testing
+@testable import GymRat
+
+struct GymRatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
